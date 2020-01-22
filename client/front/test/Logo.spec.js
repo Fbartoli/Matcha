441892b73ca5d291d6ad56ff2b95eec3244bb3f0
Matthieu SAUBIN <msaubin@e3r7p23.42.fr>
2020-01-23 00:11:59 +0100

    RAS msaubin push

--- client/front/test/Logo.spec.js
+++ client/front/test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import BigHeartLogo from '@/components/BigHeartLogo.vue'
 
-describe('Logo', () => {
+describe('BigHeartLogo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(BigHeartLogo)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
