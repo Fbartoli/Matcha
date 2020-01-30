5b9f43e258245472cf8fcef957ce7e0e9373388d
Fbartoli <fbartoli@monaco.edu>
2020-01-30 17:56:36 +0100

    check if mysql connect alive

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,17 +1,39 @@
 const mysql = require('mysql');
 const CONFIG = require('../config/config');
 
-const connection = mysql.createConnection({
+const db_config = {
   host: CONFIG.db_host,
   port: CONFIG.db_port,
   user: CONFIG.db_user,
   password: CONFIG.db_password,
-  database: CONFIG.db_name,
-});
+  database: CONFIG.db_name
+};
+
+const connection = mysql.createConnection(db_config);
 connection.connect(function(res, err) {
   if (err) {
     console.log(err);
   }
   console.log('Connected!');
 });
-module.exports = connection;
+
+function handleDisconnect() {
+  let connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+module.exports = {connection,
+  handleDisconnect};
--- models/usermodel.js
+++ models/usermodel.js
@@ -1,9 +1,10 @@
-const db = require('../models/dbconnection');
+const {db, handleDisconnect} = require('../models/dbconnection');
 
 module.exports = {
   getAllusers: (req, callback) => {
     const pathname = req._parsedUrl.pathname.split('/');
     const section = pathname[1];
+    handleDisconnect();
     db.query('SELECT * from ??', [section], function(error, result) {
       if (error) {
         return callback(error, null);
@@ -13,6 +14,7 @@ module.exports = {
     });
   },
   addUser: (post, callback) => {
+    handleDisconnect();
     db.query('INSERT INTO users (username, name, surname, email, password, confirmation) VALUES (?, ?, ?, ?, ?, ?)', post, function(error, result) {
       if (error) {
         return callback(error, null);
@@ -22,6 +24,7 @@ module.exports = {
     });
   },
   findOneUser: (field, info, callback) => {
+    handleDisconnect();
     db.query('SELECT * FROM users WHERE ??=?', [field, info], function(error, result) {
       if (error) {
         return callback(error, null);
@@ -31,6 +34,7 @@ module.exports = {
     });
   },
   activate: (confirmation, callback) => {
+    handleDisconnect();
     db.query('UPDATE users SET active = ? WHERE confirmation = ?', [1, confirmation], function(error, result) {
       if (error) {
         return callback(error, null);
@@ -40,6 +44,7 @@ module.exports = {
     });
   },
   updateUser: (id, info, callback) => {
+    handleDisconnect();
     db.query('INSERT INTO users (mobile, bio, birth_date, country, city, postal_code, gender_id, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', info, function(error, result) {
       if (error) {
         return callback(error, null);
@@ -49,6 +54,7 @@ module.exports = {
     });
   },
   updateConfirmation: (confirmation, callback) => {
+    handleDisconnect();
     db.query('UPDATE users SET confirmation = ? WHERE confirmation =?', ['0', confirmation], function(error, result) {
       if (error) {
         return callback(error, null);
