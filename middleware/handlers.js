086ffa7a2e7b4e469b3ca22b6db6ddaf42f348fe
Fbartoli <fbartoli@monaco.edu>
2020-01-31 18:54:11 +0100

    fixing api res

--- middleware/handlers.js
+++ middleware/handlers.js
@@ -16,31 +16,38 @@ const jwtCheck = (req, res, callback) => {
   }
 
   let payload = 0;
+  let apiResult = {};
   try {
-  payload = jwt.verify(token, jwtKey);
-  console.log(payload);
-  console.log(payload.exp - Math.round(Number(new Date()) / 1000));
+    payload = jwt.verify(token, jwtKey);
+    console.log(payload);
+    console.log(payload.exp - Math.round(Number(new Date()) / 1000));
   } catch (err) {
     if (err instanceof jwt.JsonWebTokenError) {
       console.log(err);
-      res.set('Content-Type', 'text/html');
+      apiResult.meta = {
+        error: err,
+      };
+      apiResult.data = [];
 
-      return res.send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/login"></head></html>');
+      return res.send(apiResult);
     }
   }
   const nowUnixSeconds = Math.round(Number(new Date()) / 1000);
   if (payload.exp - nowUnixSeconds < 30) {
-    res.set('Content-Type', 'text/html');
+    apiResult.meta = {
+      error: 'Token expired',
+    };
+    apiResult.data = [];
 
-    return res.status(400).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/login"></head></html>');
+    return res.send(apiResult);
   }
-  const newToken = jwt.sign({username: payload.username}, jwtKey, {
+  const newToken = jwt.sign({ username: payload.username }, jwtKey, {
     algorithm: 'HS256',
     expiresIn: jwtExpirySeconds
   });
 
   // Set the new token as the users `token` cookie
-  res.cookie('token', newToken, {maxAge: jwtExpirySeconds});
+  res.cookie('token', newToken, { maxAge: jwtExpirySeconds });
   callback(req, res);
 };
 
