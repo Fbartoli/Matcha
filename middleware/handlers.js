eb360505b7fdf27e8307a644d0bb0694bd96ce73
Fbartoli <fbartoli@monaco.edu>
2020-01-31 18:58:31 +0100

    fixing renewal token

--- middleware/handlers.js
+++ middleware/handlers.js
@@ -7,16 +7,21 @@ const jwtExpirySeconds = CONFIG.jwt_expiration;
 const jwtCheck = (req, res, callback) => {
   // We can obtain the session token from the requests cookies, which come with every request
   const token = req.cookies.token;
+  let apiResult = {};
 
   // if the cookie is not set, return an unauthorized error
   if (!token) {
-    res.set('Content-Type', 'text/html');
+    console.log(err);
+    apiResult.meta = {
+      error: err,
+    };
+    apiResult.data = [];
 
-    return res.status(401).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/login"></head></html>');
+    return res.send(apiResult);
   }
 
   let payload = 0;
-  let apiResult = {};
+
   try {
     payload = jwt.verify(token, jwtKey);
     console.log(payload);
