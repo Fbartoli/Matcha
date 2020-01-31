71b326c01f7317ad229ffa9d01e14a0581d74d43
Fbartoli <fbartoli@monaco.edu>
2020-01-31 18:48:37 +0100

    curly braces test

--- router.js
+++ router.js
@@ -4,34 +4,34 @@ const user = require('./controllers/user');
 const path = require('path');
 const jwtcheck = require('./middleware/handlers');
 
-router.get('/', function(req, res) {
-  res.send('Welcome to the Matcha API');
-  });
+router.get('/', function (req, res) {
+  res.send({ msg: 'Welcome to the Matcha API' });
+});
 
-router.get('/activate', function(req, res) {
+router.get('/activate', function (req, res) {
   user.activate(req, res);
-  });
+});
 
 router.route('/register')
-  .get(function(req, res) {
-  res.sendFile('index.html', {root: path.join(__dirname)});
+  .get(function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname) });
   })
   .post(user.addUser);
 
 router.route('/login')
-  .get(function(req, res) {
-  res.sendFile('login.html', {root: path.join(__dirname)});
+  .get(function (req, res) {
+    res.sendFile('login.html', { root: path.join(__dirname) });
   })
   .post(user.checkPassword);
 
 router.route('/users')
-  .get(function(req, res) {
-  jwtcheck(req, res, user.getAllusers);
+  .get(function (req, res) {
+    jwtcheck(req, res, user.getAllusers);
   });
 
 router.route('/user')
-  .get(function(req, res) {
-  jwtcheck(req, res, user.getUser);
-});
+  .get(function (req, res) {
+    jwtcheck(req, res, user.getUser);
+  });
 
 module.exports = router;
